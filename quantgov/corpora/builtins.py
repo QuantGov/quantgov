f540d171471818929145b334ce44e17f43cb90e3
Jonathan Nelson <jnelson@mercatus.gmu.edu>
2017-11-06 15:49:29 -0500

    complexity builtins

--- quantgov/corpora/builtins.py
+++ quantgov/corpora/builtins.py
@@ -6,6 +6,9 @@ import collections
 
 import quantgov
 
+from nltk.corpus import stopwords
+from textblob import Word
+
 commands = {}
 
 
@@ -93,3 +96,120 @@ class OccurrenceCounter():
 
 
 commands['count_occurrences'] = OccurrenceCounter
+
+
+class ShannonEntropy():
+
+    cli = quantgov.utils.CLISpec(
+        help='Shannon Entropy',
+        arguments=[
+            quantgov.utils.CLIArg(
+                flags=('--word_pattern', '-wp'),
+                kwargs={
+                    'help': 'regular expression defining a "word"',
+                    'type': re.compile,
+                    'default': re.compile(r'\b\w+\b')
+                }
+            )
+            quantgov.utils.CLIArg(
+                flags=('--stopwords', '-sw'),
+                kwargs={
+                    'help': 'stopwords to ignore',
+                    'default': set(stopwords.words('english'))
+                }
+            )
+        ]
+
+    )
+
+    @staticmethod
+    def get_columns(args):
+        return ('shannon_entropy',)
+
+    @staticmethod
+    def process_document(doc, word_pattern, stopwords):
+        words = word_pattern.findall(doc.text)
+        lemmas = [
+            lemma for lemma in (
+                Word(word).lemmatize() for word in words
+            )
+            if lemma not in stopwords
+        ]
+        counts = collections.Counter(lemmas)
+        return round(sum(
+            -(count / len(lemmas) * math.log(count / len(lemmas), 2))
+            for count in counts.values()
+        ), 2)
+
+
+commands['shannon_entropy'] = ShannonEntropy
+
+
+class ConditionalCounter():
+
+    cli = quantgov.utils.CLISpec(
+        help='Conditional Counter',
+        arguments=[
+            quantgov.utils.CLIArg(
+                flags=('--conditional_pattern', '-cp'),
+                kwargs={
+                    'help': 'regular expression defining a "conditional"',
+                    'type': re.compile,
+                    'default': re.compile(
+                        r'\b(if|but|except|provided|when|where|whenever|unless|notwithstanding'
+                        r'|in\s+the\s+event|in\s+no\s+event)\b')
+                }
+            )
+        ]
+    )
+
+    @staticmethod
+    def get_columns(args):
+        return ('conditional_count',)
+
+    @staticmethod
+    def process_document(doc, conditional_pattern):
+        return len(pattern.findall(' '.join((doc.text).splitlines())))
+
+
+commands['conditional_count'] = ConditionalCounter
+
+
+class SentenceLength():
+
+    cli = quantgov.utils.CLISpec(
+        help='Sentence Length',
+        arguments=[
+            quantgov.utils.CLIArg(
+                flags=('--word_pattern', '-wp'),
+                kwargs={
+                    'help': 'regular expression defining a "word"',
+                    'type': re.compile,
+                    'default': re.compile(r'\b\w+\b')
+                }
+            )
+            quantgov.utils.CLIArg(
+                flags=('--sentence_pattern', '-sp'),
+                kwargs={
+                    'help': 'regular expression defining a "sentence"',
+                    'type': re.compile,
+                    'default': re.compile(r'[A-Z][^\.!?]*[\.!?]')
+                }
+            )
+        ]
+    )
+
+    @staticmethod
+    def get_columns(args):
+        return ('sentence_length',)
+
+    @staticmethod
+    def process_document(doc, word_pattern, sentence_pattern):
+        sentences = sentence_pattern.findall(doc)
+        total_length = 0
+        for sentence in sentences:
+            total_length += len(word_pattern.findall(sentence))
+        return total_length / len(sentences)
+
+
+commands['sentence_length'] = SentenceLength
