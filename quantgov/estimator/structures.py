265cc83b06c4c3f37510cecf6a614364b222a80d
Michael Gasvoda <mgasvoda@mercatus.gmu.edu>
2017-12-19 14:13:42 -0500

    adjusting lda model, adding parameters for grid search

--- quantgov/estimator/structures.py
+++ quantgov/estimator/structures.py
@@ -5,6 +5,33 @@ Useful structures for evaluating and training estimators
 """
 import collections
 import joblib as jl
+from sklearn.base import BaseEstimator, TransformerMixin
+from six import iteritems
+from decorator import decorator
+import re
+
+try:
+    from spacy.lang.en.stop_words import STOP_WORDS
+    from gensim.corpora import Dictionary
+    import gensim
+    spacy = True
+except ImportError:
+    spacy = None
+    gensim = None
+
+
+@decorator
+def check_spacy(func, *args, **kwargs):
+    if spacy is None:
+        raise RuntimeError('Must install spacy to use {}'.format(func))
+    return func(*args, **kwargs)
+
+
+@decorator
+def check_gensim(func, *args, **kwargs):
+    if gensim is None:
+        raise RuntimeError('Must install gensim to use {}'.format(func))
+    return func(*args, **kwargs)
 
 
 class _PersistanceMixin(object):
@@ -85,3 +112,38 @@ class CandidateModel(
             parameter values to test as values
     """
     pass
+
+
+class QGLdaModel(BaseEstimator, TransformerMixin):
+    def __init__(self, word_regex=r'\b[A-z]{2,}\b', stop_words=STOP_WORDS):
+        self.stop_words = stop_words
+        self.word_regex = re.compile(word_regex)
+
+    def transform(self, driver):
+        return self.model.transform(driver.stream)
+
+    def create_corpus(self, driver):
+        return [self.dictionary.doc2bow([i.group(0)
+                for i in self.word_regex.finditer(doc.text)])
+                for doc in driver.stream()]
+
+    @check_gensim
+    @check_spacy
+    def fit(self, driver, alpha=None, eta=None, num_topics=None, passes=None):
+        self.dictionary = Dictionary([[i.group(0)
+                                      for i in self.word_regex
+                                        .finditer(doc.text)]
+                                      for doc in driver.stream()])
+        stop_ids = [self.dictionary.token2id[stopword] for stopword
+                    in self.stop_words if stopword in self.dictionary.token2id]
+        once_ids = [tokenid for tokenid, docfreq in
+                    iteritems(self.dictionary.dfs) if docfreq == 1]
+        self.dictionary.filter_tokens(stop_ids + once_ids)
+        self.corpus = self.create_corpus(driver)
+        self.model = gensim.models.ldamodel.LdaModel(self.corpus,
+                                                     id2word=self.dictionary,
+                                                     alpha=alpha
+                                                     eta=eta,
+                                                     num_topics=num_topics,
+                                                     passes=passes)
+        return self
